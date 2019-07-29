19e8e51753e843a65157e0ef026bd4da0b65307f
subh <subhendukundu14@gmail.com>
2019-07-29 23:29:32 +0530

    feat: google login init

--- README.md
+++ README.md
@@ -1,1 +1,108 @@
 # react-native-simple-google-login
+
+![npm (tag)](https://img.shields.io/npm/v/react-native-simple-google-login/latest) [![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com) ![GitHub top language](https://img.shields.io/github/languages/top/subhendukundu/react-native-simple-google-login) ![David](https://img.shields.io/david/subhendukundu/react-native-simple-google-login) [![publish size](https://badgen.net/packagephobia/publish/react-native-simple-google-login)](https://badgen.net/packagephobia/publish/react-native-simple-google-login)
+
+A `<GoogleLogin />` component for react-native no native code added light weight library to make user Google login. No setup, no linking required, use it like a component.
+
+This module uses [Google OAuth2](https://developers.google.com/identity/protocols/OAuth2).
+
+## Installation
+
+```sh
+npm i --save react-native-simple-google-login
+```
+
+or
+
+```sh
+yarn add react-native-simple-google-login
+```
+
+### Setup Instructions
+
+1. Go to the [Google Developers Console](https://console.developers.google.com/apis/dashboard)
+2. Select your project or create a new one (and then select it)
+3. Create a service account for your project
+  - In the sidebar on the left, expand __APIs & auth__ > __Credentials__
+  - Click blue "Add credentials" button
+  - Select the "OAuth client ID" option
+  - Select the "other" key type option
+  - Click blue "Create" button
+  - Save the Client_id, that's all you need (__it is the only copy!__)
+
+## Props
+
+| name | desc | type | default
+| --- | --- | --- | --- |
+| `credentialsDetails` | Google client details | object | {redirectUrl: '', clientId:''}
+| `scope` | scope for the login which will be included in access token | string | `'profile email openid'`
+| `getAccessToken` | Callback for access token when token received | function | `undefined`
+| `getUserDetails` | Callback for user details when received | function | `undefined`
+
+## Usage
+
+```javascript
+import React, { useState } from 'react';
+import GoogleLogin, { userDetails, accessToken } from 'react-native-simple-google-login';
+
+export default function App() {
+  const clientId = 'YOUR_CLIENT_ID';
+  const GOOGLE_REDIRECT_URI = 'http://localhost';
+  const [ isLoggedIn, setLoggedIn ] = useState(false);
+
+  function _onPressLoginButton() {
+    if(isLoggedIn) {
+      //do something else
+    } else {
+      setLoggedIn(true);
+    }
+  }
+
+  function getAccessToken(token) {
+      console.log(token, 'User logged in, token received');
+      setLoggedIn(false);
+  }
+
+  function getUserDetails(data) {
+      console.log(data, 'User details received');
+  }
+
+  function _onPressDetailsButton() {
+    console.log(userDetails, 'stored user details in async storage');
+    console.log(accessToken, 'stored token details in async storage');
+  }
+
+  return (
+    <View style={{flex: 1}}>
+      <GoogleLogin
+        credentialsDetails={{
+          redirectUrl: GOOGLE_REDIRECT_URI,
+          clientId,
+        }}
+        getAccessToken={getAccessToken}
+        getUserDetails={getUserDetails}
+      />
+      <TouchableOpacity onPress={_onPressLoginButton}>
+        <Text>Login with Google</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={_onPressDetailsButton}>
+        <Text>Get user details</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+```
+
+## Dependency
+
+[react-native-webview](https://github.com/react-native-community/react-native-webview)
+[react-native-webview](https://github.com/react-native-community/async-storage)
+
+### Contribute
+
+Help to make it better! Please see [CONTRIBUTING.md](https://github.com/subhendukundu/react-native-simple-google-login/blob/master/CONTRIBUTING.md) for more details.
+
+License
+
+----
+MIT License
---
+++ index.js
@@ -0,0 +1,189 @@
+import React, { useState } from 'react';
+import { Modal } from 'react-native';
+import { WebView } from 'react-native-webview';
+import AsyncStorage from '@react-native-community/async-storage';
+
+const GOOGLE_AUTHORIZATION_URL = 'https://accounts.google.com/o/oauth2/v2/auth';
+const GOOGLE_TOKEN_URL = 'https://www.googleapis.com/oauth2/v4/token';
+const GOOGLE_PROFILE_URL = 'https://www.googleapis.com/userinfo/v2/me';
+
+function parse(querystring) {
+    querystring = querystring.substring(querystring.indexOf('?')+1).split('&');
+    const params = {};
+    let pair = void 0;
+    const d = decodeURIComponent;
+    for (let i = querystring.length - 1; i >= 0; i--) {
+      pair = querystring[i].split('=');
+      params[d(pair[0])] = d(pair[1] || '');
+    }
+    return params;
+}
+
+/**
+ * Get query string
+ *
+ * @param   {*}   query   query object (any object that Object.entries() can handle)
+ * @returns {string}      query string
+ */
+
+function querystring(obj) {
+    const keyValuePairs = [];
+    for (const key in obj) {
+      if (obj[key] !== undefined) {
+          keyValuePairs.push(encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]));
+      }
+    }
+    return keyValuePairs.join('&');
+}
+
+class ReactNativeGoogleLogin {
+    constructor() {
+        this.accessToken = '';
+        this.userDetails = null;
+        this.sessionValid = false;
+        this.checkValidToken();
+        this.getUserDetailsFromAsyncStorage();
+    }
+
+    async function checkAccessToken(token) {
+        return fetch(`https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=${token}`).then(res => res.json());
+    }
+
+    getUserDetailsFromAsyncStorage = async () => {
+        try {
+            const data = await AsyncStorage.getItem('@storage_GoogleSimpleLoginUserData');
+            if (data !== null) {
+              this.userDetails = data;
+            }
+        } catch(e) {
+            console.error('Error fetching toekn in async-storage');
+        }
+    }
+
+    checkValidToken = async () => {
+        try {
+            const token = await AsyncStorage.getItem('@storage_GoogleSimpleLoginToken');
+            if (token !== null) {
+              const data = await this.checkAccessToken(token);
+              if(data.access_type && !data.error_description) {
+                this.accessToken = token;
+                this.sessionValid = true;
+              }
+            }
+        } catch(e) {
+            console.error('Error fetching toekn in async-storage');
+        }
+    }
+
+    storeData = async (type, data) => {
+        try {
+          await AsyncStorage.setItem(, data);
+        } catch (e) {
+          console.error('Error saving toekn in async-storage');
+        }
+    }
+
+    GoogleLogin(props) {
+        const { credentialsDetails, getAccessToken, getUserDetails, scope } = props;
+        const { redirectUrl = 'https://localhost', clientId } = credentialsDetails;
+        const loginState = this.accessToken && this.sessionValid;
+        const [ isLoggedIn, setLoggedIn ] = useState(loginState);
+        
+        const urlParams = {
+          response_type: 'code',
+          redirect_uri: redirectUrl,
+          client_id: clientId,
+          scope: scope || 'profile email openid',
+        };
+        const authUrl = `${GOOGLE_AUTHORIZATION_URL}?${querystring(urlParams)}`;
+      
+        async function fetchAccessTokens(code) {
+            const params = {
+                code,
+                client_id: clientId,
+                redirect_uri: redirectUrl,
+                grant_type: 'authorization_code',
+            };
+            const response = await fetch(`${GOOGLE_TOKEN_URL}?${querystring(params)}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                }
+            });
+            return response.json();
+        }
+      
+        async function fetchUserDetails(accessToken) {
+            const response = await fetch(GOOGLE_PROFILE_URL, {
+                method: "GET", 
+                headers: {
+                    'Content-Type': 'application/json; charset=UTF-8',
+                    'Authorization': `Bearer ${accessToken}`,
+                }
+            });
+            return response.json();
+        }
+        function handleNavigation (url) {
+          const query = parse(url);
+          if (query) {
+            if (query.code) {
+              setLoggedIn(true);
+              fetchAccessTokens(query.code).then((tokenData) => {
+                const token = tokenData.access_token;
+                this.storeData('@storage_GoogleSimpleLoginToken', token);
+                if (typeof getAccessToken === 'function') {
+                  getAccessToken(token);
+                }
+                this.accessToken = token;
+                fetchUserDetails(token).then((userData) => {
+                  this.userDetails = userData;
+                  this.storeData('@storage_GoogleSimpleLoginUserData', userData);
+                  if (typeof getUserDetails === 'function') {
+                      getUserDetails(userData);
+                  }
+                });
+              }).catch(e => console.log(e));
+            }
+          }
+        }
+        function onNavigationStateChange(e) {
+          handleNavigation(e.url);
+        }
+      
+        return (
+            <>
+            {
+                isLoggedIn ? null :
+                <Modal>
+                    <WebView
+                        useWebKit
+                        sharedCookiesEnabled
+                        source={{ uri: authUrl }}
+                        mixedContentMode="compatibility"
+                        javaScriptEnabled
+                        javaScriptEnabledAndroid
+                        bounces
+                        userAgent = "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"
+                        domStorageEnabled
+                        thirdPartyCookiesEnabled
+                        originWhitelist={['*']}
+                        onNavigationStateChange={onNavigationStateChange}
+                    />
+                </Modal>
+            }
+            </>
+        );
+    }
+}
+const {
+    GoogleLogin,
+    userDetails,
+    accessToken,
+    sessionValid
+} = new ReactNativeGoogleLogin();
+export default GoogleLogin;
+export {
+    userDetails,
+    accessToken,
+    sessionValid
+};
